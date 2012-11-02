f3eaa633db8d322cf6eb2f00d0297833d9556678
Ben Perry <bap63@cornell.edu>
2012-11-01 20:54:53 -0400

    Update
    Everything should be there. Might be missing some of the english course
    listings from Spring of 2012

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 4940-Cornell
 ============
+
+Instructions:
+1.Run selenium-server jar file by opening a command line and entering:
+	java -jar selenium-server-standalone-2.25.0.jar
+
+2. Open a second command line and run main.py. Should open a firefox browser. Enter your NETID.
+
+3. Return to python command line, hit enter. Browser will run and scrap data.
+
+4. Check logs for errors. A file is generated at the begining of each run. The name is the timestamp of start time.
--- main.py
+++ main.py
@@ -3,36 +3,91 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 import util
 import time
-#driver.implicitly_wait(1)
+import datetime
+import csv
 
+def fetchAll(all):
+    dept = util.print_stale_dropdown(driver,"ctl00_ContentPlaceHolder1_deptListDropDownList")
+    #dept.reverse()
+    for d in dept:
+        print "Starting dept: " + d
+        driver.get(uriBook)
+        time.sleep(1)
+        dropID = "ctl00_ContentPlaceHolder1_deptListDropDownList"
+        util.click_stale_dropdown(driver, dropID, d)
+        time.sleep(1)
+        util.print_table(driver,d)
 
+        if all:
+            try:
+                cl = len(courseLinks)
+                i = 0
+                try:
+                    while cl > i: 
+                        for _ in xrange(10): 
+                            try:
+                                link = util.fetch_stale_table(driver)
+                                cur = link[i]
+                            except IndexError:
+                                print "Regenerating book list..."
+                                time.sleep(1)        
+                        cur.click()
+                        util.fetch_books(driver)
+                        driver.get(uriBook)
+                        util.click_stale_dropdown(driver, dropID, d)
+                        i+=1
+                except Exception as e:
+                    error = e + " on " + d + "\n"
+                    print error
+                    log.write(error) 
+            except:
+                log.write("Could not find books for " + d + "\n")
+    
+def fetchSingle(depts):
+    
+    for d in depts:
+        print "Starting dept: " + d
+        driver.get(uriBook)
+        driver.implicitly_wait(1)
+        dropID = "ctl00_ContentPlaceHolder1_deptListDropDownList"
+        util.click_stale_dropdown(driver, dropID, d)
+        driver.implicitly_wait(1)
+        courseLinks = util.fetch_stale_table(driver)
+        try:
+            cl = len(courseLinks)
+            i = 0
+            try:
+                while cl > i: 
+                    for _ in xrange(10): 
+                        try:
+                            link = util.fetch_stale_table(driver)
+                            cur = link[i]
+                        except IndexError:
+                            print "Regenerating book list..."
+                            time.sleep(1)       
+                    cur.click()
+                    util.fetch_books(driver)
+                    driver.get(uriBook)
+                    util.click_stale_dropdown(driver, dropID, d)
+                    i+=1
+            except Exception as e:
+                error = e + " on " + d + "\n"
+                print error
+                log.write(error) 
+        except:
+            log.write("Could not find books for " + d + "\n")
+    
+
+now = datetime.datetime.now()	
+fn = now.strftime("%Y-%m-%d_%H-%M") + ".txt"
+log = open("Logs/"+fn, "w")
 uriBook = 'http://textbooks.store.cornell.edu/courseVerifications.aspx'
 driver = webdriver.Firefox()
 driver.get(uriBook)
-
 raw_input("Login with NETID. Then press enter to continue...")
-
 driver.get(uriBook)
 
-dropID = "ctl00_ContentPlaceHolder1_deptListDropDownList"
-util.click_stale_dropdown(driver, dropID, 'INFO - INFORMATION SCIENCE')
-time.sleep(1)
 
-#util.print_table(driver,'INFO - INFORMATION SCIENCE')
-
-courseLinks = util.fetch_stale_table(driver)
-cl = len(courseLinks)
-i = 0
-while cl > i: 
-    for _ in xrange(10): 
-        try:
-            link = util.fetch_stale_table(driver)
-            cur = link[i]
-        except IndexError:
-            print "Regenerating book list..."
-            time.sleep(1)         
-    cur.click()
-    util.fetch_books(driver)
-    driver.get(uriBook)
-    util.click_stale_dropdown(driver, dropID, 'INFO - INFORMATION SCIENCE')
-    i+=1
+depts = ['ENGL - ENGLISH']
+fetchSingle(depts)
+#fetchAll(False)
