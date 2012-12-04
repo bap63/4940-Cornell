2b201dbb1abed23eb2aeed7e95e23b818e13b6f1
Derek Harden <dmh264@cornell.edu>
2012-12-03 22:12:22 -0500

    Message about changes

---
+++ web stuff/buy.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+  <title>BookScore Results</title>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+  <link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+
+<body>
+<div id="wrapper">
+    <div id="header">
+
+        <div id="logo">
+        <p>BookScore.zxq.net</p>
+        </div>
+
+            
+    <div id="content">
+    
+     <p>&nbsp</p>
+     <p>&nbsp</p>
+     
+      	<?php
+			print("<h2>Top Results For ".$_POST['search'].":</h2>");
+		?>
+      
+      	<p>&nbsp</p>
+        
+        <div id="results">
+        	<p>&nbsp</p>
+        	<h3>book one</h3>
+        	<h3>book two</h3>
+        	<h3>book three</h3>
+        	<p>&nbsp</p>
+        </div>
+        
+        <p>&nbsp</p>
+        <p>&nbsp</p>
+
+    </div>
+    
+    <div id="footer">  
+    
+    	<ul id="navigation">
+
+                <li class="on"><a href="index.html">Home</a></li>
+
+        </ul>
+                    
+    </div>
+    
+</div>
+
+</body>
+</html>
