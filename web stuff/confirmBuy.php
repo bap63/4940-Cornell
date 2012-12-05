079e538c2a96432b44c0e2ab00c9619c71f4825a
Derek Harden <dmh264@cornell.edu>
2012-12-04 22:45:37 -0500

    Message about changes

--- web stuff/confirmBuy.php
+++ web stuff/confirmBuy.php
@@ -4,7 +4,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
-  <title>BookScore Results</title>
+  <title>Purchase Details</title>
   <meta http-equiv="content-type" content="text/html; charset=utf-8" />
   <link rel="stylesheet" href="style.css" type="text/css" />
 </head>
@@ -25,46 +25,25 @@
      
       	<?php
       	
-      	if ($_POST['group1']=='buy'){
       	
-			print("<h2>Top Results For ".$_POST['search'].":</h2>");
-			print("
-			
-			<p>&nbsp</p>
-        
-        		<div id=\"results\">
-        			<p>&nbsp</p>
-        			<a href=\"confirmBuy.php\"><h3>book one</h3></a>
-        			<a href=\"confirmBuy.php\"><h3>book two</h3></a>
-        			<a href=\"confirmBuy.php\"><h3>book three</h3></a>
-        			<p>&nbsp</p>
-        		</div>
-        
-        		<p>&nbsp</p>
-        		<p>&nbsp</p>
-        
-        ");
-			
-		}
-		else {
 		
 			print("
 				<p>this is where our sell stuff will go<p>
 				<p>&nbsp</p>
 			");
+			print("<input title=\"email\" name=\"email\" value=\"email address\" size=\"80\" onclick=\"if ( value == 'email address' ) { value = ''; }\"/>");
 			
-			print("<h2>Sell Books Related to ".$_POST['search']."</h2>");
 			print("
-				<h3>Comments About Your Book</h3>
+				<h3>Meetup instructions</h3>
 				<form method=\"post\" action=\"\">
 					<textarea name=\"comments\" cols=\"40\" rows=\"8\">
 					</textarea><br>
-					<input type=\"submit\" value=\"Sell This Book\" />
+					<input type=\"submit\" value=\"Submit\" />
 				</form>
 				
 				");
 		
-		}
+		
 			
 		?>
       
