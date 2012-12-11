2c95dc5e30b460f9424fc6d47254b342b3d351c6
krs228 <krs228@cornell.edu>
2012-12-10 21:01:31 -0500

    testing API

--- web stuff/api/main.php
+++ web stuff/api/main.php
@@ -1,9 +1,17 @@
 <?php
 
 //TODO:Finish
+print_r( get_request_headers() );
 
-
-
+function get_request_headers() {
+    $headers = array();
+    foreach($_SERVER as $key => $value) {
+        if(substr($key, 0, 5) == 'HTTP_') {
+            $headers[str_replace(' ', '-', ucwords(str_replace('_', ' ', strtolower(substr($key, 5)))))] = $value;
+        }
+    }
+    return $headers;
+}
 
 
 ?>
