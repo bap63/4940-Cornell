4af9b8df9b184ce7ca1d4d71a4309e086b75d07e
krs228 <krs228@cornell.edu>
2012-12-11 01:05:09 -0500

    API supports JSON/XML/CSV

--- web stuff/api/main.php
+++ web stuff/api/main.php
@@ -1,44 +1,68 @@
 <?php
 
-//TODO:Finish
 $request_headers = getBestSupportedMimeType();
-//print_r($request_headers);
 
-/*function get_request_headers() {
-    $headers = array();
-    foreach($_SERVER as $key => $value) {
-        if(substr($key, 0, 5) == 'HTTP_') {
-            $headers[str_replace(' ', '-', ucwords(str_replace('_', ' ', strtolower(substr($key, 5)))))] = $value;
-        }
+function returnMimeType($rh, $books){
+    if(key($rh) == "application/json"){
+        returnJSON($books);
     }
-    return $headers;
+    else if(key($rh) == "application/xml"){
+    	returnXML($books);
+    }
+    else if(key($rh) == "text/csv"){
+    	returnCSV($books);
+    }
+    else{
+	//Unsupported MIME type send HTTP 406
+    	header(':', true, 406);
+    }
 }
 
-if(key($request_headers) == 'text/html'){
-    print_r("HTML");
+function returnJSON($books) {
+    header('Content-type: application/json');
+    $json = json_encode($books);
+    print_r($json);
 }
-else if(key($request_headers) == "text/json"){
-    print_r("JSON");
+function returnXML($books) {
+    header('Content-type: application/xml');
+    $xml = new SimpleXMLElement('<xml/>');
+    foreach($books as $book){
+	//create the root element
+	$root = $xml->addChild('book');
+	$root->addChild('isbn', $book['isbn']);
+	$root->addChild('title', $book['title']);
+	$root->addChild('author', $book['author']);
+	$root->addChild('publisher', $book['publisher']);
+	$root->addChild('price', $book['price']);
+    }
+    print_r($xml->asXML());
 }
-else if(key($request_headers) == "application/xml"){
-    print_r("XML");
-}
-else if(key($request_headers) == "text/csv"){
-    print_r("CSV");
-}
-else{
-    print_r("Unsupported MIME type");
-}*/
+function returnCSV($books) {
+    $fileName = 'bookList.csv';
+    header("Content-type: text/csv");
+    header("Content-Disposition: attachment; filename={$fileName}");
+    //set up filehandler to output a .csv file
+    $fh = @fopen( 'php://output', 'w' );
+    $headerDisplayed = false;
 
-function returnJSON() {
-}
-function returnXML() {
-}
-function returnCSV() {
+    foreach ( $books as $book ) {
+    // Add a header row if it hasn't been added yet
+        if ( !$headerDisplayed ) {
+            // Use the keys from $data as the titles
+            fputcsv($fh, array_keys($book));
+            $headerDisplayed = true;
+        }
+        // Put the data into the stream
+        fputcsv($fh, $book);
+    }
+    // Close the file
+    fclose($fh);
+    exit;
 }
 
 
-//function adapted from Maciej Łebkowski from http://stackoverflow.com/questions/1049401/how-to-select-content-type-from-http-accept-header-in-php
+//function adapted from Maciej Łebkowski
+//http://stackoverflow.com/questions/1049401/how-to-select-content-type-from-http-accept-header-in-php
 function getBestSupportedMimeType() {
     // Values will be stored in this array
     $AcceptTypes = Array ();
